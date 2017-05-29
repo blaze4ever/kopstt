9a45faccb33cc306eb5b6cc53deafb8ccc58c5fd
mikolaj <m.jakubowski@hydro-partner.pl>
2017-05-29 14:56:17 +0200

    working with database creator

---
+++ Kopstt.Core/Structure/DBStructure.cs
@@ -0,0 +1,15 @@
+﻿using Kopstt.Core.LocatDatabase;
+
+namespace Kopstt.Core.Structure
+{
+    public abstract class DBStructure
+    {
+        private Database _db;
+        public DBStructure(Database db)
+        {
+            _db = db;
+        }
+
+        public abstract void CreateTable();
+    }
+}
---
+++ Kopstt.Core/Structure/Tables/Tasks.cs
@@ -0,0 +1,17 @@
+﻿namespace Kopstt.Core.LocalDatabase.Structure
+{
+    public class Tasks : DBStructure
+    {
+        public string CreateQuery()
+        {
+            if (_db == null)
+            {
+                return;
+            }
+
+            var query = "CREATE table IF NOT EXISTS tasks (name varchar(20), added datetime)";
+            var command = new SQLiteCommand(query, _db);
+            command.ExecuteNonQuery();
+        }
+    }
+}
